cb98a8ac4c68acd7bdbc8e5dded17c0eb8417b67
dhatri <160123575+Dhatripatel06@users.noreply.github.com>
2025-06-04 18:48:36 +0530

    Add files via upload

---
+++ 12_in_notin.py
@@ -0,0 +1,11 @@
+subject = ['statistics','socialscience','maths','economics']
+isget = input('Enter your faviourate subject ')
+print(isget in subject)  # T
+print(isget not in subject)  # F
+
+
+
+img ="jpg png jpeg"
+isget = input('Enter your image name ')
+print(isget in img)  # T
+print(isget not in img)  # F
---
+++ 13_is.py
@@ -0,0 +1,12 @@
+d1 = 10
+d2 = 50
+d3 = 20
+
+res = d1 is d2
+print("result =",res,id(d1),id(d2),id(d3))
+
+res = d1 is not d2
+print("result =",res,id(d1),id(d2),id(d3)) # definr the address
+
+
+# is and not is 
