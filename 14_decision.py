f2f9983d45ba23953ac8c93082f6b3ed60d5a592
dhatri <160123575+Dhatripatel06@users.noreply.github.com>
2025-06-05 18:36:15 +0530

    Add files via upload

---
+++ 14_decision.py
@@ -0,0 +1,51 @@
+# if statement-
+r = int(input("Enter the redius"))
+
+if r <0:
+    r = -r
+area = r*r*r
+print("The area of the circle is", area)
+
+
+
+
+#if else
+
+age = int(input("Enter the age "))
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+
+
+# elif 
+grade = int(input("Enter the marks "))
+
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+else:
+    print("Below C")
+
+
+
+#   neasted if
+username = input("Enter username  ")
+password = input("Enter the password  ")
+otp = input("Enter the otp  ")   # if  int apply then it get logical error 
+
+if username == "admin":
+    if password == "secret123":
+        if otp == "123456":
+            print("Access granted to admin dashboard")
+        else:
+            print("Valid username and password, but wrong OTP")
+    else:
+        print("Valid username, but wrong password")
+else:
+    print("Invalid username")
